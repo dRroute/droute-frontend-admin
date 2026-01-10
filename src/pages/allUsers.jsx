fd88b177f40e5276c6417e55d0c9931617995e9c
Jaysingh Patel <jay229413@gmail.com>
2026-01-10 22:45:27 +0530

    Add spacing for Create User button in All Users component

--- src/pages/allUsers.jsx
+++ src/pages/allUsers.jsx
@@ -86,6 +86,7 @@ function AllUsers() {
               onClick={() => setCreateUserModalOpen(true)}
               className="bg-cyan-700 hover:bg-gray-600 text-white text-sm px-3 py-1 rounded-sm shadow-sm transition-all duration-200"
             >
+              
               Create User
             </button>
           </div>
@@ -107,7 +108,7 @@ function AllUsers() {
                 <tbody>
                   {tableData
                     .filter((row) => {
-                      if (!search.trim()) return true; // Show all if search empty
+                      if (!search.trim()) return true; 
                       const s = search.toLowerCase();
                       return (
                         (row.name && row.name.toLowerCase().includes(s)) ||
